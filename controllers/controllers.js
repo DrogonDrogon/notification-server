cc7fe0da19c0f25932f93e048869582b1a51d51f
waltershub <waltershub@gmail.com>
2017-09-10 18:24:22 -0400

    notifications

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -3,14 +3,49 @@ const db = require('../db/index');
 const Expo = require('expo-server-sdk');
 
 const expo = new Expo();
+const axios = require('axios');
 
 exports.notification = (req, res) => {
-  console.log(req.body);
   db.child(`users/${req.body.userid}`)
     .once('value')
-    .then((user) => {
-      console.log(user.val());
+    .then((data) => {
+      const user = data.val();
+      // Create a new Expo SDK client
+
+
+      // Create the messages that you want to send to clents
+      const messages = [];
+
+
+      // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+      // axios.post('https://exp.host/--/api/v2/push/send', {
+      //   to: user.token,
+      //   sound: 'default',
+      //   body: req.body.message,
+      //   data: { withSome: 'data' },
+      // }).then((res) => {
+      //   if (res) console.log('status', res);
+      //   // console.log(res);
+      // });
+
+
+      messages.push({
+        to: user.token,
+        sound: 'default',
+        body: req.body.message,
+        data: { withSome: 'data' },
+      });
+      const chunks = expo.chunkPushNotifications(messages);
+      // console.log('chunk', chunk);
+      for (const chunk of chunks) {
+        try {
+          expo.sendPushNotificationsAsync(chunk);
+          console.log('chunk', chunk);
+        } catch (error) {
+          console.error(error);
+        }
+      }
     });
-  // console.log(db);
+
   res.send('test');
 };
